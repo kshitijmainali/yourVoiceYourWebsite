de4ecb1ef12b60af492dad2d38de4f63bea379f7
kshitij <mainalikshitij@gmail.com>
2021-02-18 18:40:54 +0545

    proper setup in vscode complete

---
+++ main.py
@@ -0,0 +1,16 @@
+# listen to the voice and convert to text
+import voice
+
+spokenText = voice.listener()
+print(spokenText)
+
+
+# tokenization
+# convert word to lower case and stemming
+# exclude puctuation
+# find out the nessecary command
+# create and update the json file
+# end the voice command session
+# make HTML file according to json file formed
+# render the HTML file thus created
+# now continue the steps
---
+++ voice.py
@@ -0,0 +1,44 @@
+
+import speech_recognition as sr
+import pyaudio as pa
+
+r1 = sr.Recognizer()
+
+
+def listener():
+    with sr.Microphone() as source:
+        print('speak now')
+        audio1 = r1.listen(source)
+        return r1.recognize_google(audio1)
+
+
+'''
+import speech_recognition as sr
+import pyaudio as pa
+import webbrowser as wb
+
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+r3 = sr.Recognizer()
+
+
+with sr.Microphone() as source : 
+    print('speak now')
+    global audio1
+    audio1 = r3.listen(source)
+print('thing is: '+r3.recognize_google(audio1))
+if 'edureka' in r2.recognize_google(audio1):
+    r2 = sr.Recognizer()
+    url = 'https://www.edureka.co/'
+    with sr.Microphone() as source:
+        print('search your query')
+        audio2 = r2.listen(source)
+        try: 
+            get = r2.recognize_google(audio2)
+            print(get)
+            wb.get().open_new(url+get)
+        except sr.UnknownValueError:
+            print('error')
+        except sr.RequestError as e:
+            print('failed')
+'''
